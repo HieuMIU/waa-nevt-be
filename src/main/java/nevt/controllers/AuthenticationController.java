8f19eff1bfb8ae937dec49f2d54eb86abe03e50c
Thang Nguyen <nguyendinhthanght@gmail.com>
2023-11-16 16:58:16 -0600

    add cross origin

--- src/main/java/nevt/controllers/AuthenticationController.java
+++ src/main/java/nevt/controllers/AuthenticationController.java
@@ -4,16 +4,14 @@ import nevt.dto.account.JwtAuthenticationResponse;
 import nevt.dto.account.SignInRequest;
 import nevt.dto.account.SignUpRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import nevt.services.AuthenticationService;
 
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "*")
 public class AuthenticationController {
 
     @Autowired
